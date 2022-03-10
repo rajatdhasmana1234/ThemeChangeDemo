7d077392829bb8c45158e0b1e161cf81ade6a606
Rajat Dhasmana <96765608+rajatdhasmana1234@users.noreply.github.com>
2022-03-10 11:23:48 +0530

    initial commit

---
+++ ThemeChangeDemo/ThemeChangeDemo/ThemeChangeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeChangeDemoApp.swift
+//  ThemeChangeDemo
+//
+//  Created by RAJAT DHASMANA on 10/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ThemeChangeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
